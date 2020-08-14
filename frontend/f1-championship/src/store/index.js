887e1bb27a58ba40941a3a482b2d979aa169df0d
brunobarros <brunobarros@brunobarross-MacBook-Pro.local>
2020-08-14 22:08:46 +0100

    use Vuex to map the actions and state

---
+++ frontend/f1-championship/src/store/index.js
@@ -0,0 +1,86 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { circuits, teams } from "../circuits";
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    selectedRace: 0,
+    fasterLap: new Array(circuits.length),
+    results: [],
+    circuits,
+    teams
+  },
+  getters: {
+    winnersOfCurrentRace(state) {
+      return state.results.filter((winner) => winner.race == state.selectedRace);
+    },
+    getDriverByNumber: state => (driverNumber) => {
+      for (let index = 0; index < state.teams.length; index++) {
+        const team = state.teams[index];
+        for (let index = 0; index < team.drivers.length; index++) {
+          const driver = team.drivers[index];
+          if (driver.num == driverNumber) {
+            return driver;
+          }
+        }
+      }
+    },
+    getTeamDriverByDriverNumber: (state) => (driverNumber) => {
+      for (let index = 0; index < state.teams.length; index++) {
+        const team = state.teams[index];
+        for (let index = 0; index < team.drivers.length; index++) {
+          const driver = team.drivers[index];
+          if (driver.num == driverNumber) {
+            return team;
+          }
+        }
+      }
+    },
+    driverAlreadyOnThisRace: (state) => (driverNumber) => {
+      const drivers = state.results
+        .filter((result) => result.race == state.selectedRace)
+        .map((result) => result.driver);
+      return drivers.includes(driverNumber);
+    },
+    alreadyTenDrivers: (state) => (race) =>{
+      return state.results.filter((winner) => winner.race == race).length == 10;
+    },
+  },
+  mutations: {
+    SET_CURRENT_RACE(state, raceIndex) {
+      state.selectedRace = raceIndex;
+    },
+    SET_DRIVER_ON_RACE(state, driverNumber) {
+      const driver = {
+        race: state.selectedRace,
+        driver: driverNumber,
+      };
+
+      state.results.push(driver);
+    },
+    REMOVE_DRIVER_FROM_RACE(state, driverNumber) {
+      const index = state.results.findIndex(
+        (res) => res.driver == driverNumber && res.race == state.selectedRace
+      );
+
+      if (index > -1) {
+        state.results.splice(index, 1);
+      }
+    }
+  },
+  actions: {
+    selectRace({ commit }, raceIndex) {
+      commit("SET_CURRENT_RACE", raceIndex);
+    },
+    insertDriverToRace({ commit }, driverNumber) {
+      commit("SET_DRIVER_ON_RACE",
+        driverNumber
+      );
+    },
+    removeDriverOfRace({ commit }, driverNumber) {
+      commit("REMOVE_DRIVER_FROM_RACE", driverNumber);
+    }
+  }
+})
